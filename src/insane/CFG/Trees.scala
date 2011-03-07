19a7a853e77168fb2526d55f64bb6fda7b9497cc
Etienne Kneuss <etienne.kneuss@epfl.ch>
2011-03-07 13:06:57 +0000

    Some CFG basis
    git-svn-id: https://larasvn.epfl.ch/svn-repos/projects/guru/trunk/insane@1716 155c431c-efaa-44cd-a91a-e98606b5e0f9

--- src/insane/CFG/Trees.scala
+++ src/insane/CFG/Trees.scala
@@ -3,8 +3,42 @@ package insane.CFG
 
 object Trees {
 
-  sealed abstract class Tree {
+  private var _nextID = 0;
 
+  def nextID = {
+    _nextID += 1;
+    _nextID
   }
 
+  sealed abstract class Tree extends ASTBound {
+    val uniqueID = nextID
+  }
+
+  sealed abstract class Statement extends Tree
+
+  case class AssignVal(val r: Ref, val v: SimpleValue) extends Statement
+  case class AssignMethodCall(val r: Ref, val receiver: SimpleValue, val method: MethodRef, val args: Seq[Seq[SimpleValue]]) extends Statement
+  case class AssignNew(val r: Ref, val f: ClassRef, val args: Seq[SimpleValue]) extends Statement
+
+  case class Assert(val v: SimpleValue) extends Statement
+
+  case class Branch(val cond: BranchCondition) extends Statement
+  case class Skip() extends Statement
+
+  sealed abstract class SimpleValue extends Statement
+  // TODO Ref, This?
+
+  sealed abstract class StaticValue extends SimpleValue
+  case class Long(value: Long) extends StaticValue
+  case class Float(value: Float) extends StaticValue
+  case class String(value: String) extends StaticValue
+  case class True() extends StaticValue
+  case class Any() extends StaticValue
+  case class False() extends StaticValue
+  case class Null() extends StaticValue
+
+  sealed abstract class BranchCondition extends Tree
+  case object Maybe extends BranchCondition
+
+  // TODO: ClassRef, MethodRef
 }
