7200ddb582ed5580a6bad1630dbcae443f7ec649
Etienne Kneuss <etienne.kneuss@epfl.ch>
2011-03-24 14:46:04 +0000

    Nice errors
    git-svn-id: https://larasvn.epfl.ch/svn-repos/projects/guru/trunk/insane@1836 155c431c-efaa-44cd-a91a-e98606b5e0f9

--- src/insane/utils/Reporter.scala
+++ src/insane/utils/Reporter.scala
@@ -6,8 +6,8 @@ import scala.tools.nsc.Global
 class Reporter(global: Global) {
   def fatalError(msg: String) = Predef.error(msg)
 
-  def info(msg: String) = println("[insane] "+msg)
+  def info(msg: String) = println("insane> "+msg)
 
-  def error(msg: String) = info("Error: "+msg)
-  def warn(msg: String) = info("Warning: "+msg)
+  def error(msg: String) = info("["+Console.RED+"error"+Console.RESET+"] "+msg)
+  def warn(msg: String) = info("["+Console.YELLOW+"warn"+Console.RESET+"] "+msg)
 }
