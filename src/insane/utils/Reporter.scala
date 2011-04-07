e1096c31e82758a70838be58ddf415adf9b1f67c
Etienne Kneuss <etienne.kneuss@epfl.ch>
2011-04-07 15:32:34 +0000

    Print stacktrace in debugmode
    git-svn-id: https://larasvn.epfl.ch/svn-repos/projects/guru/trunk/insane@1977 155c431c-efaa-44cd-a91a-e98606b5e0f9

--- src/insane/utils/Reporter.scala
+++ src/insane/utils/Reporter.scala
@@ -4,11 +4,37 @@ package utils
 import scala.tools.nsc.Global
 
 class Reporter(global: Global) {
+  var debugMode = true
+
   def fatalError(msg: String) = Predef.error(msg)
 
   def info(msg: String) = println("insane> "+msg)
 
-  def error(msg: String) = info("["+Console.RED+"error"+Console.RESET+"] "+Console.RED+msg+Console.RESET)
-  def warn(msg: String) = info("["+Console.YELLOW+"warn"+Console.RESET+"] "+Console.YELLOW+msg+Console.RESET)
-  def title(msg: String) = info(Console.BLUE+Console.BOLD+msg+Console.RESET)
+  def error(msg: String) = {
+    info("["+Console.RED+"error"+Console.RESET+"] "+Console.RED+msg+Console.RESET)
+    if (debugMode) {
+      debugDetails()
+    }
+  }
+
+  def warn(msg: String) = {
+    info("["+Console.YELLOW+"warn"+Console.RESET+"] "+Console.YELLOW+msg+Console.RESET)
+    if (debugMode) {
+      debugDetails()
+    }
+  }
+
+  private def debugDetails() = {
+    val sw = new java.io.StringWriter
+    new Exception().printStackTrace(new java.io.PrintWriter(sw))
+
+    val trace = sw.toString.split("\n").drop(3).mkString("\n")
+
+    println(trace)
+
+  }
+
+  def title(msg: String) = {
+    info(Console.BLUE+Console.BOLD+msg+Console.RESET)
+  }
 }
