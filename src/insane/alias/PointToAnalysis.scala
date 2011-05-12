d374acc67c944db8a1df12e44698430614f34c0f
Etienne Kneuss <etienne.kneuss@epfl.ch>
2011-05-12 11:15:55 +0000

    Some changes on point-to graphs
    git-svn-id: https://larasvn.epfl.ch/svn-repos/projects/guru/trunk/insane@2253 155c431c-efaa-44cd-a91a-e98606b5e0f9

--- src/insane/alias/PointToAnalysis.scala
+++ src/insane/alias/PointToAnalysis.scala
@@ -14,34 +14,66 @@ trait PointToAnalysis {
     val name = "Point-to Analysis"
 
     sealed abstract class PTField
-    case class PTFieldSymbol(sym: Symbol) extends PTField
+    case class PTRefSymbol(ref: CFG.Ref) extends PTField
     case object PTArrayFields extends PTField
 
-    class PTEnv(val ptGraph: PointToGraph[PTField]) extends DataFlowEnvAbs[PTEnv, CFG.Statement] {
-      var isBottom = false
+    type INode = PTInsNode[PTField]
+    type LNode = PTLoadNode[PTField]
+    type PNode = PTParamNode[PTField]
+    type Node  = PTNodeAbs[PTField]
 
-      def copy = new PTEnv(ptGraph)
+    case class PTEnv(ptGraph: PointToGraph[PTField]) extends DataFlowEnvAbs[PTEnv, CFG.Statement] {
 
       def union(that: PTEnv) = {
-        new PTEnv(ptGraph union that.ptGraph)
+        PTEnv(ptGraph union that.ptGraph)
       }
 
-      override def equals(that: Any) = that match {
-        case a: PTEnv =>
-          (a.ptGraph == ptGraph) && (a.isBottom == isBottom)
-        case _ => false
+      def getL(ref: CFG.Ref): Set[Node] = {
+        ptGraph.locState(PTRefSymbol(ref))
       }
-    }
 
-    object PTBaseEnv extends PTEnv(new PointToGraph[PTField]) {
-      isBottom = true
+      def setL(ref: CFG.Ref, nodes: Set[Node]): PTEnv = {
+        PTEnv(ptGraph.copy(locState = ptGraph.locState + (PTRefSymbol(ref) -> nodes)))
+      }
+
+      def newInsideNode(label: Int): (PTEnv, INode) = {
+        val n = new INode(label)
+        (PTEnv(ptGraph + n), n)
+      }
+
+      def copy = this
     }
 
     class PointToTF extends TransferFunctionAbs[PTEnv, CFG.Statement] {
       type Env = PTEnv
-      def apply(st: CFG.Statement, oldEnv: Env): Env = {
-        val env = oldEnv.copy
 
+      def apply(st: CFG.Statement, oldEnv: Env): Env = {
+        var env = oldEnv
+        st match {
+          case av: CFG.AssignVal => av.v match {
+            case r2: CFG.Ref =>
+              env = env.setL(av.r, env.getL(r2))
+            case n: CFG.Null =>
+              env = env.setL(av.r, Set())
+            case _ =>
+              // ignore
+          }
+
+          case an: CFG.AssignNew =>
+            val (newEnv, n) = env.newInsideNode(an.uniqueID)
+            env = newEnv.setL(an.r, Set(n))
+            // TODO: elements
+
+          case aa: CFG.AssignArray =>
+            val (newEnv, n) = env.newInsideNode(aa.uniqueID)
+            env = newEnv.setL(aa.r, Set(n))
+            // TODO: elements
+
+          case ac: CFG.AssignCast =>
+            env = env.setL(ac.r, env.getL(ac.rhs))
+
+          case _ =>
+        }
         env
       }
     }
