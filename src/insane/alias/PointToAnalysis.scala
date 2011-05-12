9b4f8f9766646901254b78a1c1221e486b750a99
Etienne Kneuss <etienne.kneuss@epfl.ch>
2011-05-12 11:16:08 +0000

    Store PT results in contexts, Also, move PTEnv out of the phase scope
    git-svn-id: https://larasvn.epfl.ch/svn-repos/projects/guru/trunk/insane@2272 155c431c-efaa-44cd-a91a-e98606b5e0f9

--- src/insane/alias/PointToAnalysis.scala
+++ src/insane/alias/PointToAnalysis.scala
@@ -11,119 +11,117 @@ trait PointToAnalysis extends PointToGraphsDefs {
   import global._
   import PointToGraphs._
 
+  case class PTEnv(ptGraph: PointToGraph,
+                 locState: Map[CFG.Ref, Set[Node]],
+                 iEdges: Set[IEdge],
+                 oEdges: Set[OEdge],
+                 eNodes: Set[Node],
+                 rNodes: Set[Node],
+                 isBottom: Boolean) extends DataFlowEnvAbs[PTEnv, CFG.Statement] {
+
+    def this(isBottom: Boolean = false) = this(new PointToGraph(), Map().withDefaultValue(Set()), Set(), Set(), Set(), Set(), isBottom)
+
+    def union(that: PTEnv) = {
+      copy(ptGraph union that.ptGraph,
+        (locState.keySet ++ that.locState.keySet).map(k => k -> (locState(k)++that.locState(k))).toMap,
+        iEdges union that.iEdges,
+        oEdges union that.oEdges,
+        eNodes union that.eNodes,
+        rNodes union that.rNodes,
+        false)
+    }
+
+    def reachableNodes(from: Set[Node], via: Set[Edge]): Set[Node] = {
+      var res = from
+      var queue = from
+
+      while(!queue.isEmpty) {
+        val n = queue.head
+        queue = queue.tail
+
+        val toAdd = via collect { case Edge(v1, _, v2) if (v1 == n) && !(res contains v2) => v2 }
+
+        queue = queue ++ toAdd
+        res = res ++ toAdd
+      }
+
+      res
+    }
+
+    def escapingNodes: Set[Node] = {
+      val from = (ptGraph.vertices.collect { case n: LNode => n; case n: PNode => n }) ++ eNodes + GBNode
+
+      reachableNodes(from, Set() ++ iEdges)
+    }
+
+    def processLoad(vRef: CFG.Ref, vFrom: CFG.Ref, field: Field, pPoint: Int) = {
+      val nodesFromVFrom = iEdges.collect { case Edge(v1,l,v2) if (l == field) && (getL(vFrom) contains v1) => v2 }
+      val escapings      = escapingNodes intersect getL(vFrom)
+
+      if (escapings.isEmpty) {
+        // If nodes of vFrom are not escaping the current scope:
+        copy(locState = locState + (vRef -> nodesFromVFrom))
+      } else {
+        val lNode = LNode(pPoint)
+        addNode(lNode).setL(vRef, (nodesFromVFrom + lNode)).addOEdges(escapings, field, Set(lNode))
+      }
+    }
+
+    def setL(ref: CFG.Ref, nodes: Set[Node]) = {
+      copy(locState = locState + (ref -> nodes), isBottom = false)
+    }
+
+    def getL(ref: CFG.Ref): Set[Node] = locState(ref)
+
+    def addNode(node: Node) =
+      copy(ptGraph = ptGraph + node, isBottom = false)
+
+    def addOEdges(lv1: Set[Node], field: Field, lv2: Set[Node]) = {
+      var newGraph = ptGraph
+      var oEdgesNew = oEdges
+      for (v1 <- lv1; v2 <- lv2) {
+        val e = OEdge(v1, field, v2)
+        newGraph += e
+        oEdgesNew += e
+      }
+      copy(ptGraph = newGraph, oEdges = oEdgesNew, isBottom = false)
+    }
+
+    def addIEdges(lv1: Set[Node], field: Field, lv2: Set[Node]) = {
+      var newGraph = ptGraph
+      var iEdgesNew = iEdges
+      for (v1 <- lv1; v2 <- lv2) {
+        val e = IEdge(v1, field, v2)
+        newGraph += e
+        iEdgesNew += e
+      }
+      copy(ptGraph = newGraph, iEdges = iEdgesNew, isBottom = false)
+    }
+
+    def addENodes(nodes: Set[Node]) = {
+      copy(ptGraph = ptGraph ++ nodes, eNodes = eNodes ++ nodes, isBottom = false)
+    }
+
+    def setReturnNodes(ref: CFG.Ref) = {
+      val nodes = getL(ref)
+      copy(ptGraph = ptGraph ++ nodes, rNodes = nodes, isBottom = false)
+    }
+
+    def addGlobalNode() = {
+      copy(ptGraph = ptGraph + GBNode, isBottom = false)
+    }
+
+    def duplicate = this
+  }
+
+  object BottomPTEnv extends PTEnv(true)
+
   class PointToAnalysisPhase extends SubPhase {
     val name = "Point-to Analysis"
 
-    var functionsToGraph = Map[AbsFunction, Env]()
+    class PointToTF(cfg: FunctionCFG) extends TransferFunctionAbs[PTEnv, CFG.Statement] {
 
-    case class Env(ptGraph: PointToGraph,
-                   locState: Map[CFG.Ref, Set[Node]],
-                   iEdges: Set[IEdge],
-                   oEdges: Set[OEdge],
-                   eNodes: Set[Node],
-                   rNodes: Set[Node],
-                   isBottom: Boolean) extends DataFlowEnvAbs[Env, CFG.Statement] {
-
-      def this(isBottom: Boolean = false) = this(new PointToGraph(), Map().withDefaultValue(Set()), Set(), Set(), Set(), Set(), isBottom)
-
-      def union(that: Env) = {
-        Env(ptGraph union that.ptGraph,
-            (locState.keySet ++ that.locState.keySet).map(k => k -> (locState(k)++that.locState(k))).toMap,
-            iEdges union that.iEdges,
-            oEdges union that.oEdges,
-            eNodes union that.eNodes,
-            rNodes union that.rNodes,
-            false)
-      }
-
-      def reachableNodes(from: Set[Node], via: Set[Edge]): Set[Node] = {
-        var res = from
-        var queue = from
-
-        while(!queue.isEmpty) {
-          val n = queue.head
-          queue = queue.tail
-
-          val toAdd = via collect { case Edge(v1, _, v2) if (v1 == n) && !(res contains v2) => v2 }
-
-          queue = queue ++ toAdd
-          res = res ++ toAdd
-        }
-
-        res
-      }
-
-      def escapingNodes: Set[Node] = {
-        val from = (ptGraph.vertices.collect { case n: LNode => n; case n: PNode => n }) ++ eNodes + GBNode
-
-        reachableNodes(from, Set() ++ iEdges)
-      }
-
-      def processLoad(vRef: CFG.Ref, vFrom: CFG.Ref, field: Field, pPoint: Int) = {
-        val nodesFromVFrom = iEdges.collect { case Edge(v1,l,v2) if (l == field) && (getL(vFrom) contains v1) => v2 }
-        val escapings      = escapingNodes intersect getL(vFrom)
-
-        if (escapings.isEmpty) {
-          // If nodes of vFrom are not escaping the current scope:
-          copy(locState = locState + (vRef -> nodesFromVFrom))
-        } else {
-          val lNode = LNode(pPoint)
-          addNode(lNode).setL(vRef, (nodesFromVFrom + lNode)).addOEdges(escapings, field, Set(lNode))
-        }
-      }
-
-      def setL(ref: CFG.Ref, nodes: Set[Node]): Env = {
-        copy(locState = locState + (ref -> nodes), isBottom = false)
-      }
-
-      def getL(ref: CFG.Ref): Set[Node] = locState(ref)
-
-      def addNode(node: Node) =
-        copy(ptGraph = ptGraph + node, isBottom = false)
-
-      def addOEdges(lv1: Set[Node], field: Field, lv2: Set[Node]) = {
-        var newGraph = ptGraph
-        var oEdgesNew = oEdges
-        for (v1 <- lv1; v2 <- lv2) {
-          val e = OEdge(v1, field, v2)
-          newGraph += e
-          oEdgesNew += e
-        }
-        copy(ptGraph = newGraph, oEdges = oEdgesNew, isBottom = false)
-      }
-
-      def addIEdges(lv1: Set[Node], field: Field, lv2: Set[Node]) = {
-        var newGraph = ptGraph
-        var iEdgesNew = iEdges
-        for (v1 <- lv1; v2 <- lv2) {
-          val e = IEdge(v1, field, v2)
-          newGraph += e
-          iEdgesNew += e
-        }
-        copy(ptGraph = newGraph, iEdges = iEdgesNew, isBottom = false)
-      }
-
-      def addENodes(nodes: Set[Node]) = {
-        copy(ptGraph = ptGraph ++ nodes, eNodes = eNodes ++ nodes, isBottom = false)
-      }
-
-      def setReturnNodes(ref: CFG.Ref): Env = {
-        val nodes = getL(ref)
-        copy(ptGraph = ptGraph ++ nodes, rNodes = nodes, isBottom = false)
-      }
-
-      def addGlobalNode(): Env = {
-        copy(ptGraph = ptGraph + GBNode, isBottom = false)
-      }
-
-      def duplicate = this
-    }
-
-    object BottomEnv extends Env(true)
-
-    class PointToTF(cfg: FunctionCFG) extends TransferFunctionAbs[Env, CFG.Statement] {
-
-      def apply(st: CFG.Statement, oldEnv: Env): Env = {
+      def apply(st: CFG.Statement, oldEnv: PTEnv): PTEnv = {
         var env = oldEnv
 
         def getNodes(sv: CFG.SimpleValue): Set[Node] = sv match {
@@ -140,7 +138,7 @@ trait PointToAnalysis extends PointToGraphsDefs {
             afr.obj match {
               case CFG.SymRef(symbol) if symbol.isModule =>
                 // If we have r = obj.field where obj is a global object, we have that r is pointing to GLB
-                env = env.addGlobalNode.setL(afr.r, Set(GBNode))
+                env = env.addGlobalNode().setL(afr.r, Set(GBNode))
               case _ =>
                 env = env.processLoad(afr.r, afr.obj, SymField(afr.field), afr.uniqueID)
             }
@@ -155,12 +153,16 @@ trait PointToAnalysis extends PointToGraphsDefs {
             }
 
           case aam: CFG.AssignApplyMeth => // r = o.v(..args..)
+            callTargets.get(aam) match {
+              case Some((targets, exhaust)) =>
 
+              case _ =>
+                println("No target found for "+aam)
+            }
 
-          case an: CFG.AssignNew => // r = new A(.. args ..)
+          case an: CFG.AssignNew => // r = new A
             val iNode = INode(an.uniqueID)
             env = env.addNode(iNode).setL(an.r, Set(iNode))
-            // We need to call the constructor as well
 
           case aa: CFG.AssignArray =>
             // TODO: Implement rare use-cases
@@ -177,8 +179,8 @@ trait PointToAnalysis extends PointToGraphsDefs {
 
     def analyze(fun: AbsFunction) {
       val cfg       = fun.cfg.get
-      val bottomEnv = BottomEnv;
-      var baseEnv   = new Env();
+      val bottomEnv = BottomPTEnv;
+      var baseEnv   = new PTEnv();
 
       // 1) We add 'this' and argument nodes
       for ((a, i) <- (cfg.thisReferences.toSeq ++ fun.CFGArgs).zipWithIndex) {
@@ -189,7 +191,7 @@ trait PointToAnalysis extends PointToGraphsDefs {
 
       // 2) We run a fix-point on the CFG
       val ttf = new PointToTF(cfg)
-      val aa = new DataFlowAnalysis[Env, CFG.Statement](bottomEnv, baseEnv, settings)
+      val aa = new DataFlowAnalysis[PTEnv, CFG.Statement](bottomEnv, baseEnv, settings)
 
       reporter.info("Analyzing "+fun.symbol.fullName+"...")
 
@@ -198,7 +200,7 @@ trait PointToAnalysis extends PointToGraphsDefs {
       // 3) We retrieve the exit CFG
       val e = aa.getResult(cfg.exit).setReturnNodes(cfg.retval)
 
-      functionsToGraph += fun -> e
+      pointToEnvs += fun.symbol -> e
 
       val name = fun.symbol.fullName;
       if (settings.dumpPTGraph(name)) {
--- src/insane/analysis/Context.scala
+++ src/insane/analysis/Context.scala
@@ -13,5 +13,6 @@ trait Context extends Functions {
   val classDescendentGraph  = new ClassDescendentGraph
   // Contains, for every possible Apply, the list of symbols that it potentially points to, and whether it is exhaustive
   var callTargets           = Map[CFG.AssignApplyMeth, (Set[Symbol], Boolean)]()
+  var pointToEnvs           = Map[Symbol, PTEnv]()
   var purityResults         = Map[Symbol, PurityInfo]()
 }
