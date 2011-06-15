578b5b76154838b5ebbd77e834be9426f8e1c45e
Etienne Kneuss <colder@php.net>
2011-06-15 17:41:27 +0200

    Implement serialization of types and symbols

--- src/insane/storage/Serialization.scala
+++ src/insane/storage/Serialization.scala
@@ -16,15 +16,32 @@ trait SerializationHelpers {
 
   abstract class Serialization {
     var is: InputStream
-    var os: InputStream
+    var os: OutputStream
 
     class Helpers {
+      def write(str: String) {
+        os.write(str.getBytes("UTF-8"))
+      }
+
+      def read(len: Int): String = {
+        val arr = new Array[Byte](len)
+
+        val gotLen = is.read(arr, 0, len)
+
+        if (gotLen == len) {
+          new String(arr)
+        } else {
+          sys.error("Unserialization error,  expected "+len+" chars, got "+gotLen)
+        }
+      }
+
       def readUntil(delim: Byte): String = {
         var res = new StringBuffer
         var c = is.read()
 
-        while(c != delim) {
+        while(c != delim && c >= 0) {
           res append c
+          c = is.read()
         }
 
         res.toString
@@ -40,22 +57,68 @@ trait SerializationHelpers {
     }
 
     object Symbol extends Helpers {
-      def read(): RealSymbol = {
-        sys.error("todo")
+      def readClassSymbol(): RealSymbol = {
+        read(3) match {
+          case "cl:" =>
+            // Class Symbol
+            definitions.getClass(readUntil(':')) 
+          case "mc:" =>
+            // ModuleClass Symbol
+            definitions.getModule(readUntil(':')) 
+          case _ =>
+            sys.error("Unnexpected class symbol type!")
+        }
       }
 
-      def write(s: RealSymbol) {
-        sys.error("todo")
+      def readSymbol(): RealSymbol = {
+        read(3) match {
+          case "cl:" =>
+            // Class Symbol
+            definitions.getClass(readUntil(':')) 
+          case "mc:" =>
+            // ModuleClass Symbol
+            definitions.getModule(readUntil(':')) 
+          case "te:" =>
+            // Term Symbol
+            val cl = readClassSymbol
+            cl.tpe.decls.lookup(readUntil(':'))
+        }
+      }
+
+      def writeSymbol(s: RealSymbol) {
+        if (s.isModuleClass) {
+          write("mc:"+uniqueClassName(s)+":")
+        } else if (s.isClass) {
+          write("cl:"+uniqueClassName(s)+":")
+        } else if (s.isTerm) {
+          write("te:"+uniqueClassName(s.owner)+":"+s.name+":")
+        } else {
+          debugSymbol(s)
+          sys.error("Unnexpected kind of symbol here!")
+        }
       }
     }
 
     object Type extends Helpers {
-      def read(): RealType = {
-        sys.error("todo")
+      def readType(): RealType = {
+        read(3) match {
+          case "at:" =>
+            // Array Type
+            arrayType(readType())
+          case "st:" =>
+            // Simple Type
+            Symbol.readClassSymbol().tpe
+        }
       }
 
-      def write(t: RealType) {
-        sys.error("todo")
+      def writeType(t: RealType) {
+        t match {
+          case TypeRef(NoPrefix, definitions.ArrayClass, List(tpe)) =>
+            write("at:")
+            writeType(tpe)
+          case tpe =>
+            write("st:"+Symbol.writeSymbol(tpe.typeSymbol))
+        }
       }
     }
 
