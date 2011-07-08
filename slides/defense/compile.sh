98758b2b57f04f67c9b9ec95c6bd0feebc38d1e6
Etienne Kneuss <colder@php.net>
2011-07-08 18:51:06 +0200

    Some slides

---
+++ slides/defense/compile.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ ! $1 ]
+then
+    filename="slides"
+else
+    filename=$1
+fi
+
+rubber --pdf --force ${filename}
+
+# pdflatex $filename | grep "arning"
+#bibtex $filename | grep "arning"
+# pdflatex $filename | grep "arning"
+# pdflatex $filename | grep "arning"
+# dvipdf $filename
